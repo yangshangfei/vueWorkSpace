0ac582cf26a3c77348f64cad8e3ca3b414658211
yangshangfei <545205374@qq.com>
2018-04-04 10:37:53 +0800

    vue路由

---
+++ src/components/vueTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="vueTest">
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "vueTest",
+    data() {
+      return {
+        msg: "这是一个vueTest"
+      }
+    }
+  }
+</script>
