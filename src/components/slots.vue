c9a177778f699b683871d3782a14ed4cc3a97458
yangshangfei <545205374@qq.com>
2018-04-04 17:14:22 +0800

    vue

---
+++ src/components/slots.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slot">
+    <h1>我是子组件标题</h1>
+    <slot>没有内容的时候我会显示</slot>
+    <h2>{{msg}}</h2>
+    <slot text="我是作用域插槽"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'slots',
+    data () {
+      return {
+        msg: '插槽分发内容'
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import vueTest from '@/components/vueTest'
+import slots from '@/components/slots'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/vueTest',
       name: 'vueTest',
       component: vueTest
+    },
+    {
+      path: '/slots',
+      name: 'slots',
+      component: slots
     }
   ]
 })
